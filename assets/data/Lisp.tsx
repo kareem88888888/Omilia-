dc06e8fc6fe75944d8cf23121ec92d01ac32625e
kareem <kareem@example.com>
2024-06-16 01:13:34 -0700

    before test component

---
+++ assets/data/Lisp.tsx
@@ -0,0 +1,249 @@
+const Lisps = [
+    {
+      id: 1,
+      name: 'المقطع الصوتي الأول',
+      image:
+        'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/peperoni.png',
+        sound:'../../../../../../assets/LSound/Recording.m4a'
+     
+    },
+    {
+      id: 2,
+      name: 'المقطع الصوتي الثاني',
+      image:
+        'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/extravaganzza.png',
+        sound:'../../../../../../assets/LSound/Recording.m4a'
+      
+    },
+    {
+      id: 3,
+      name: 'المقطع الصوتي الثالث',
+      image:
+        'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/peperoni.png',
+        sound:'../../../../../../assets/LSound/Recording.m4a'
+      
+    },
+    {
+      id: 4,
+      name: 'المقطع الصوتي الرابع',
+      image:
+        'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/margarita.png',
+        sound:'../../../../../../assets/LSound/Recording.m4a'
+      
+    },
+    {
+      id: 5,
+      name:'المقطع الصوتي الخامس',
+      image:
+        'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/veggie.png',
+        sound:'../../../../../../assets/LSound/Recording.m4a'
+      
+    },
+    {
+      id: 6,
+      name: 'المقطع الصوتي السادس',
+      image:
+        'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/hawaiin.png',
+        sound:'../../../../../../assets/LSound/Recording.m4a'
+      
+    },
+    {
+      id: 7,
+      name: 'المقطع الصوتي السابع',
+      image:
+        'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/deluxe.png',
+        sound:'../../../../../../assets/LSound/Recording.m4a'
+      
+    },
+    {
+      id: 8,
+      name: 'المقطع الصوتي الثامن',
+      image:
+        'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/veggie.png',
+        sound:'../../../../../../assets/LSound/Recording.m4a'
+      
+    },
+    {
+      id: 9,
+      name: 'المقطع الصوتي التاسع',
+      image:
+        'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/extravaganzza.png',
+        sound:'../../../../../../assets/LSound/Recording.m4a'
+      
+    },
+    {
+      id: 10,
+      name: 'المقطع الصوتي العاشر',
+      image:
+        'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/6cheese.png',
+        sound:'../../../../../../assets/LSound/Recording.m4a'
+     
+    },
+
+    {
+      id: 11,
+      name: 'المقطع الصوتي الحادي عشر',
+      image:
+        'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/peperoni.png',
+        sound:'../../../../../../assets/LSound/Recording.m4a'
+      
+    },
+    {
+      id: 12,
+      name: 'المقطع الصوتي الثاني عشر',
+      image:
+        'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/extravaganzza.png',
+        sound:'../../../../../../assets/LSound/Recording.m4a'
+      
+    },
+    {
+      id: 13,
+      name: 'المقطع الصوتي الثالث عشر',
+      image:
+        'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/peperoni.png',
+        sound:'../../../../../../assets/LSound/Recording.m4a'
+      
+    },
+    {
+      id: 14,
+      name: 'المقطع الصوتي الرابع عشر',
+      image:
+        'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/margarita.png',
+        sound:'../../../../../../assets/LSound/Recording.m4a'
+     
+    },
+    {
+      id: 15,
+      name: 'المقطع الصوتي الخامس عشر',
+      image:
+        'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/veggie.png',
+        sound:'../../../../../../assets/LSound/Recording.m4a'
+      
+    },
+    {
+      id: 16,
+      name: 'المقطع الصوتي السادس عشر',
+      image:
+        'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/hawaiin.png',
+        sound:'../../../../../../assets/LSound/Recording.m4a'
+      
+    },
+    {
+      id: 17,
+      name: 'المقطع الصوتي السابع عشر',
+      image:
+        'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/deluxe.png',
+        sound:'../../../../../../assets/LSound/Recording.m4a'
+      
+    },
+    {
+      id: 18,
+      name: 'المقطع الصوتي الثامن عشر',
+      image:
+        'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/veggie.png',
+        sound:'../../../../../../assets/LSound/Recording.m4a'
+      
+    },
+    {
+      id: 19,
+      name: 'المقطع الصوتي التاسع عشر',
+      image:
+        'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/extravaganzza.png',
+
+        sound:'../../../../../../assets/LSound/Recording.m4a'
+      
+    },
+    {
+      id: 20,
+      name: 'المقطع الصوتي العشرون',
+      image:
+        'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/6cheese.png',
+        sound:'../../../../../../assets/LSound/Recording.m4a'
+      
+    },
+
+    {
+      id: 21,
+      name: 'المقطع الصوتي الحادي و العشرون',
+      image:
+        'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/peperoni.png',
+        sound:'../../../../../../assets/LSound/Recording.m4a'
+      
+    },
+    {
+      id: 22,
+      name: 'المقطع الصوتي الثاني و العشرون',
+      image:
+        'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/extravaganzza.png',
+        sound:'../../../../../../assets/LSound/Recording.m4a'
+     
+    },
+    {
+      id: 23,
+      name: 'المقطع الصوتي الثالث و العشرون',
+      image:
+        'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/peperoni.png',
+        sound:'../../../../../../assets/LSound/Recording.m4a'
+      
+    },
+    {
+      id: 24,
+      name: 'المقطع الصوتي الرابع و العشرون',
+      image:
+        'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/margarita.png',
+        sound:'../../../../../../assets/LSound/Recording.m4a'
+     
+    },
+    {
+      id: 25,
+      name: 'المقطع الصوتي الخامس و العشرون',
+      image:
+        'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/veggie.png',
+        sound:'../../../../../../assets/LSound/Recording.m4a'
+      
+    },
+    {
+      id: 26,
+      name: 'المقطع الصوتي السادس و العشرون',
+      image:
+        'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/hawaiin.png',
+     sound:'../../../../../../assets/LSound/Recording.m4a'
+    },
+    {
+      id: 27,
+      name: 'المقطع الصوتي السابع و العشرون',
+      image:
+        'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/deluxe.png',
+        sound:'../../../../../../assets/LSound/Recording.m4a'
+      
+    },
+    {
+      id: 28,
+      name: 'المقطع الصوتي الثامن و العشرون',
+      image:
+        'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/veggie.png',
+        sound:'../../../../../../assets/LSound/Recording.m4a'
+      
+    },
+    {
+      id: 29,
+      name: 'المقطع الصوتي التاسع و العشرون',
+      image:
+        'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/extravaganzza.png',
+        sound:'../../../../../../assets/LSound/Recording.m4a'
+      
+    },
+    {
+      id: 30,
+      name: 'المقطع الصوتي الثلاثون',
+      image:
+        'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/6cheese.png',
+        sound:'../../../../../../assets/LSound/Recording.m4a'
+      
+    },
+
+
+  ];
+  
+  export default Lisps;
+  
--- src/app/starpage/SignIn/(tabs)/Test/LispTest.tsx
+++ src/app/starpage/SignIn/(tabs)/Test/LispTest.tsx
@@ -1,7 +1,40 @@
-import { StyleSheet, Text, View ,Image, ScrollView} from 'react-native'
+import { StyleSheet, Text, View ,Image, Pressable} from 'react-native'
 import React from 'react'
+import { useEffect, useState } from 'react';
+import { Audio } from 'expo-av';
+import AntDesign from '@expo/vector-icons/AntDesign';
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+
+
+const audioFile = require('../../../../../../assets/LSound/Recording.m4a');
+
 
 const LispTest = () => {
+  
+  const [isPlaying, setIsPlaying] = useState(false);
+  const[pressedone,setpressedone]=useState(false)
+  const[pressedtwo,setpressedtwo]=useState(false)
+
+
+  const handleOnPressin=()=>{
+    setpressedone(true)
+  }
+  
+  const handleOnPressout=()=>{
+    setpressedone(false)
+  }
+
+  const handleOnPressintwo=()=>{
+    setpressedtwo(true)
+  }
+  
+  const handleOnPressoutTwo=()=>{
+    setpressedtwo(false)
+  }
+
+
+
+
   return (
     <>
     <View style={{flex:1}}>
@@ -22,7 +55,48 @@ const LispTest = () => {
     <View style={{height:3,width:'100%',backgroundColor:"#DBA6F7",marginRight:20,elevation:15}}/>
 
 
+    <View style={styles.container}>
+  <View style={styles.testo}>
 
+    <Text style={{color:"#9A69D8",fontWeight:"800", padding:7}}>المقطع الصوتي الأول</Text>
+
+    <View style={{alignItems:"center",marginTop:18,flex:1}}>
+    <Text style={{fontSize:60,fontWeight:"bold"}}> سمكة</Text>
+    </View>
+
+    <View style={{alignItems:'center',marginBottom:100,flexDirection:"row-reverse",justifyContent:"center"}}>
+    <Pressable style={{margin:15}}
+    onPress={async () => {
+      const { sound } = await Audio.Sound.createAsync(audioFile);
+      sound.playAsync();
+      setIsPlaying(true) }}
+       onPressOut={handleOnPressout} onPressIn={handleOnPressin}>
+
+    <AntDesign name="playcircleo" size={35} color=  {  pressedone ? 'pink': 'black' } />
+    </Pressable>
+
+
+    <Pressable 
+    onPress={async () => {
+      const { sound } = await Audio.Sound.createAsync(audioFile);
+      sound.playAsync();
+      setIsPlaying(true)
+      }} onPressOut={handleOnPressoutTwo} onPressIn={handleOnPressintwo}
+    >
+    <MaterialIcons name="replay-circle-filled" size={40} color={  pressedtwo ? 'pink': 'black' } />
+    </Pressable>
+
+    
+   
+    </View>
+
+    
+
+
+  </View>
+
+  
+</View>
    
 
    
@@ -45,8 +119,11 @@ const styles = StyleSheet.create({
   testo:{
     width: '80%',
     aspectRatio:1,
-    backgroundColor: "red",
-    margin:5
+    backgroundColor: "white",
+    margin:5,
+    borderColor:"#DBA6F7",
+    borderWidth:1,
+    borderRadius:15,
   
   },
   container:{
