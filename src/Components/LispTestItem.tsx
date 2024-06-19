7943275a3b0855d6d2b4dda016011bcd362f339e
kareem <kareem@example.com>
2024-06-19 06:46:00 -0700

    before initial test

--- src/Components/LispTestItem.tsx
+++ src/Components/LispTestItem.tsx
@@ -5,6 +5,8 @@ import { Audio } from 'expo-av';
 import AntDesign from '@expo/vector-icons/AntDesign';
 import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 import { LispItem } from '../Types';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+
 
 export const defaultImage = require('../../assets/LSimages/1.png')
 export const defaultAudio = require('../../assets/LSound/Recording.m4a')
@@ -21,6 +23,7 @@ const LispTestItem = ({Problem}:LispTestProps) => {
     const [isPlaying, setIsPlaying] = useState(false);
   const[pressedone,setpressedone]=useState(false)
   const[pressedtwo,setpressedtwo]=useState(false)
+  const[pressedthree,setpressedthree]=useState(false)
 
 
   const handleOnPressin=()=>{
@@ -37,6 +40,10 @@ const LispTestItem = ({Problem}:LispTestProps) => {
   
   const handleOnPressoutTwo=()=>{
     setpressedtwo(false)
+  }
+
+  const handleonPressThree=()=>{
+    setpressedthree(!pressedthree)
   }
 
 
@@ -74,10 +81,30 @@ const LispTestItem = ({Problem}:LispTestProps) => {
       <MaterialIcons name="replay-circle-filled" size={40} color={  pressedtwo ? 'pink': 'black' } />
       </Pressable>
       </View>
-  
-    
+
+
+
+      
+      <View style={styles.Reco}>
+      <View style={styles.waveformContainer}>
+        <View style={styles.waveform}>
+          <Image source={require('../../assets/Voice.png')} resizeMode='stretch'/>
+        </View>
+
+        <Pressable style={styles.micButton} onPress={handleonPressThree} >
+          {pressedthree ? <FontAwesome name="square" size={30} color="white" /> : <MaterialIcons name="mic" size={50} color="white" />}
+          
+        </Pressable>
+
+      </View>
     </View>
   
+  
+    </View>
+
+
+
+    
   </View>
 
 <View style={{height:1,width:'100%',backgroundColor:"#DBA6F7",marginTop:5}}/>
@@ -91,6 +118,7 @@ export default LispTestItem
 
 const styles = StyleSheet.create({
     testo:{
+      flex:1,
         width: '80%',
         aspectRatio:1,
         backgroundColor: "white",
@@ -113,5 +141,45 @@ const styles = StyleSheet.create({
         width:'100%',
         aspectRatio:1,
         
-      }
+      },
+      Reco: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        flex:1,
+        paddingLeft:30,
+        paddingRight:10,
+        marginTop:5
+        
+        
+      },
+      waveformContainer: {
+        position: 'relative',
+        width: '100%',
+        height: 63,
+        backgroundColor: '#DBA6F7',
+        borderRadius: 15,
+        justifyContent: 'center',
+        marginBottom:160
+      },
+      micButton: {
+        position: 'absolute',
+        left: -25, // Adjust as necessary
+        width: 70,
+        height: 70,
+        borderRadius: 35,
+        backgroundColor: '#DBA6F7',
+        justifyContent: 'center',
+        alignItems: 'center',
+        elevation:8
+        
+      },
+      waveform: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        
+      },
+      waveText: {
+        color: '#fff',
+      },
 })
